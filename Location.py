2a17668e7c9bb86826c019ad5626a55718a67615
Mitch Dominici <43010365+MitchDominici@users.noreply.github.com>
2019-06-13 20:12:46 -0500

    Update Location.py

--- Location.py
+++ Location.py
@@ -1,5 +1,22 @@
 import location
 
-location = location.get_location()
 
-print (location)
+def main():
+	coordinates = location.get_location()
+	fullAddress = location.reverse_geocode(coordinates)
+	
+	street = fullAddress[0]['Street']
+	zip = fullAddress[0]['ZIP']
+	city = fullAddress[0]['City']
+	state = fullAddress[0]['State']
+	
+	address = 'Your location is' + ' ' + street + ' ' + ' ' + zip + ' ' + city + ' ' + state
+	
+	
+	return address
+	
+	
+	
+	
+if __name__=='__main__':
+	main()
