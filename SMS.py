ba4f5b1d1c364119d60946e96f1ebeddcbd752e6
Mitch Dominici <43010365+MitchDominici@users.noreply.github.com>
2019-06-13 20:13:24 -0500

    Update SMS.py

--- SMS.py
+++ SMS.py
@@ -1,15 +1,16 @@
 from twilio.rest import Client
+from GetLocation import main
 
 # Your Account SID from twilio.com/console
-account_sid = ""
+account_sid = "Your account Here"
 # Your Auth Token from twilio.com/console
-auth_token  = ""
+auth_token  = "Your Token Here"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="Phone Number", 
-    from_="Phone Number",
-    body="")
+    to="Your Phone Number Here", 
+    from_="Your Twilio Number Here",
+    body=main())
 
 print(message.sid)
